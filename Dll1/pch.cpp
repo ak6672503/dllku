a04f55fc02a3e1d8dd75602ea9032789c3b5079b
先升 呆 <ak6682928@hotmail.com>
2020-02-15 20:43:17 +0800

    添加项目文件。

---
+++ Dll1/pch.cpp
@@ -0,0 +1,38 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#define DLL1_API extern "C" _declspec(dllexport)
+#include "pch.h"
+#include <tchar.h>
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+
+
+int add(int a, int b) {
+
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+/*
+void Point::output(int x, int y)
+{
+    //返回调用者进程当前正在使用的那个窗口的句柄
+    HWND hwnd = GetForegroundWindow();
+    //获取dc
+    HDC hdc = GetDC(hwnd);
+    TCHAR buf[20] = { 0 };
+
+    _stprintf_s(buf, L"x=%d,y=%d", x, y);
+    //输出坐标
+    TextOut(hdc, 0, 0, buf, _tcslen(buf));
+    //释放dc
+    ReleaseDC(hwnd, hdc);
+
+}
+
+void Point::test()
+{
+}
+*/
---
+++ Dll1/pch.h
@@ -0,0 +1,33 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+
+#endif //PCH_H
+
+#ifdef DLL1_API
+#else
+#define DLL1_API extern "C" _declspec(dllimport)
+#endif // DLL1_API
+
+DLL1_API int add(int a, int b);
+DLL1_API int subtract(int a, int b);
+
+/*
+class  Point {
+public:
+	void DLL1_API output(int x, int y);
+	void test();
+};*/
+
+/*
+_declspec(dllimport) int add(int a, int b); //这是直接告诉编译器这是从动态链接库引入的函数
+_declspec(dllimport) int subtract(int a, int b);
+*/
